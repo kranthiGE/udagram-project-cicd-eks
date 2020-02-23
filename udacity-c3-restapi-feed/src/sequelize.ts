c1d4cfb0ec6ad03529c5665121008f0abc836482
Kranthi <achantakranthi@gmail.com>
2020-02-23 21:26:43 +0530

    with pwd

--- udacity-c3-restapi-feed/src/sequelize.ts
+++ udacity-c3-restapi-feed/src/sequelize.ts
@@ -4,10 +4,16 @@ import { config } from './config/config';
 
 const c = config.dev;
 
+console.log( `uname: ${c.username}` );
+console.log( `password: ${c.password}` );
+console.log( `database: ${c.database}` );
+console.log( `host: ${c.host}` );
+
+
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
   "username": c.username,
-  "password": c.password,
+  "password": "krandb123",
   "database": c.database,
   "host":     c.host,
 
