8e3ccefed198b788f3fc94338d1e0a242e75a50d
Kranthi <achantakranthi@gmail.com>
2020-03-10 05:03:59 +0530

    updated the host to loadbalancer url

--- udacity-c3-frontend/src/environments/environment.ts
+++ udacity-c3-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ada3ae035625d11eabdfb028ce6db618-1500691044.ap-south-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
